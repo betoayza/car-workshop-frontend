1d99e1d65379b1e3f36c1fef8b36cb831b05fd41
Alberto <aaayza@gmail.com>
2022-06-26 21:02:46 -0300

    Testing deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import * as Components from "./components/indexComponents.js";
+import * as Components from "./components/indexComponents.jsx";
 
 const App = () => {
   return (
---
+++ src/components/indexComponents.jsx
@@ -0,0 +1,41 @@
+import Home from "./Home.jsx";
+//import SignUp from "./SignUp";
+// import Login from "./Login";
+
+import AddClient from "./AddClient.jsx";
+import DeleteClient from "./DeleteClient.jsx";
+import ModifyClient from "./ModifyClient.jsx";
+import SearchClient from "./SearchClient.jsx";
+
+import AddService from "./AddService.jsx";
+import DeleteService from "./DeleteService.jsx";
+import ModifyService from "./ModifyService.jsx";
+import SearchService from "./SearchService.jsx";
+
+import AddCar from "./AddCar.jsx";
+import DeleteCar from "./DeleteCar.jsx";
+import ModifyCar from "./ModifyCar.jsx";
+import SearchCarForms from "./SearchCarForms.jsx";
+import CarList1 from "./CarList1.jsx";
+import SearchCar from "./SearchCar";
+
+import Error404 from "./Error404.jsx";
+
+export {
+  Home,
+  AddClient,
+  DeleteClient,
+  ModifyClient,
+  SearchClient,
+  AddService,
+  DeleteService,
+  ModifyService,
+  SearchService,
+  AddCar,
+  DeleteCar,
+  ModifyCar,
+  SearchCarForms,
+  SearchCar,
+  CarList1,
+  Error404,
+};
